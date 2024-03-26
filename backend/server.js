b2a0596987a5fc9c48084f323806e16493bb1086
=vishesh <=vishesh@culturro.com>
2024-03-27 01:03:41 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,20 @@ import connectDb from "./db/connectDb.js";
 import cookieParser from "cookie-parser";
 import messagesRoutes from "./routes/message.routes.js";
 import botRoutes from "./routes/bot.routes.js";
-import {app,server } from "./socket/socket.js";
+import path from "path";
+import { app, server } from "./socket/socket.js";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messagesRoutes);
-app.use('/api/bot', botRoutes);
+app.use("/api/bot", botRoutes);
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 server.listen(PORT, () => {
 	connectDb();
 	console.log("Server Running on port", PORT);
